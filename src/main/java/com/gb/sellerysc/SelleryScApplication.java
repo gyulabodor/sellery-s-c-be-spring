f10d7e9d1fa1b303a7a49b4a413e02dfb75eb244
Bodor Gyula <bodor.gyula@novaservices.hu>
2023-12-02 05:32:16 +0100

    initial commit

---
+++ src/main/java/com/gb/sellerysc/SelleryScApplication.java
@@ -0,0 +1,13 @@
+package com.gb.sellerysc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelleryScApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelleryScApplication.class, args);
+	}
+
+}
