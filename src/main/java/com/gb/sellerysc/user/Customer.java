d52a2e1796aea101efe3e6efd89e1953917a5984
Bodor Gyula <bodor.gyula@novaservices.hu>
2023-12-02 18:02:01 +0100

    add mainSection property to Customer entity

--- src/main/java/com/gb/sellerysc/user/Customer.java
+++ src/main/java/com/gb/sellerysc/user/Customer.java
@@ -3,6 +3,7 @@ package com.gb.sellerysc.user;
 import com.gb.sellerysc.expense.Expense;
 import com.gb.sellerysc.expense.ExpenseToPay;
 import com.gb.sellerysc.salary.Salary;
+import com.gb.sellerysc.shared.utils.SectionTypeEnum;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -19,8 +20,14 @@ public class Customer {
 
     @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<Expense> expenseList;
+
     @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<Salary> salaryList;
+
     @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<ExpenseToPay> expenseToPayList;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private SectionTypeEnum mainSection;
 }
