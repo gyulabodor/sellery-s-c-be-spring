e6c85338312d1ebd94374816edaee90b5eb620da
Bodor Gyula <bodor.gyula@novaservices.hu>
2023-12-02 18:51:33 +0100

    create GlobalExceptionHandler

---
+++ src/main/java/com/gb/sellerysc/shared/exception/GlobalExceptionHandler.java
@@ -0,0 +1,11 @@
+package com.gb.sellerysc.shared.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler{
+
+}
