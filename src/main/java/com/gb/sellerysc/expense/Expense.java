90d2afc01b69695dc0159dc49ae071ff4b54ceca
Bodor Gyula <bodor.gyula@novaservices.hu>
2023-12-02 17:52:38 +0100

    add payment frequency to Expense entity

--- src/main/java/com/gb/sellerysc/expense/Expense.java
+++ src/main/java/com/gb/sellerysc/expense/Expense.java
@@ -1,6 +1,7 @@
 package com.gb.sellerysc.expense;
 
 import com.gb.sellerysc.shared.utils.CurrencyEnum;
+import com.gb.sellerysc.shared.utils.FrequencyEnum;
 import com.gb.sellerysc.user.Customer;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -15,10 +16,19 @@ public class Expense {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String itemName;
+
     private Long amount;
+
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private CurrencyEnum currency;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private FrequencyEnum frequency;
+
     @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<ExpenseToPay> expenseToPayList;
 
