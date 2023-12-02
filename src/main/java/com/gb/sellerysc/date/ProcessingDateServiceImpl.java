5cddef7730ad3aa30d32d4408b72db0f45378c29
Bodor Gyula <bodor.gyula@novaservices.hu>
2023-12-02 18:26:28 +0100

    rename mapper to processingDateMapper in ProcessingDateServiceImpl

--- src/main/java/com/gb/sellerysc/date/ProcessingDateServiceImpl.java
+++ src/main/java/com/gb/sellerysc/date/ProcessingDateServiceImpl.java
@@ -8,12 +8,12 @@ import java.util.Optional;
 public class ProcessingDateServiceImpl implements ProcessingDateService{
 
     private final ProcessingDateRepository processingDateRepository;
-    private final ProcessingDateMapper mapper;
+    private final ProcessingDateMapper processingDateMapper;
     public ProcessingDateServiceImpl(
             ProcessingDateRepository processingDateRepository,
             ProcessingDateMapper processingDateMapper){
         this.processingDateRepository=processingDateRepository;
-        this.mapper=processingDateMapper;
+        this.processingDateMapper =processingDateMapper;
     }
     @Override
     public ProcessingDateData fetchProcessingDate(ProcessingDateFindRequest processingDateFindRequest) {
@@ -25,19 +25,19 @@ public class ProcessingDateServiceImpl implements ProcessingDateService{
         if (!processingDate.isPresent()){
             return null;
         }
-        return mapper.processingDateToProcessingDateData(processingDate.get());
+        return processingDateMapper.processingDateToProcessingDateData(processingDate.get());
     }
 
     @Override
     public ProcessingDateData saveProcessingDate(ProcessingDateCreateRequest processingDateCreateRequest) {
-        return mapper.processingDateToProcessingDateData(processingDateRepository
-                .save(mapper.processingDateCreateRequestToProcessingDate(processingDateCreateRequest)
+        return processingDateMapper.processingDateToProcessingDateData(processingDateRepository
+                .save(processingDateMapper.processingDateCreateRequestToProcessingDate(processingDateCreateRequest)
                 ));
     }
 
     @Override
     public List<ProcessingDateData> searchProcessingDate(ProcessingDateFindRequest processingDateFindRequest) {
-        return mapper.processingDateListToProcessingDateDataList(processingDateRepository.findAllByYearAndMonth(
+        return processingDateMapper.processingDateListToProcessingDateDataList(processingDateRepository.findAllByYearAndMonth(
                 processingDateFindRequest.getYear(),processingDateFindRequest.getMonth()
         ));
     }
