65c321d2e8a264aadba4d246dca16b8802b5f626
Bodor Gyula <bodor.gyula@novaservices.hu>
2024-04-28 06:16:15 +0200

    create CustomerData which will be the FE mapped model what I will code em

--- src/main/java/com/gb/sellerysc/user/CustomerData.java
+++ src/main/java/com/gb/sellerysc/user/CustomerData.java
@@ -1,6 +1,5 @@
 package com.gb.sellerysc.user;
 
-import com.gb.sellerysc.expense.ExpenseData;
 import com.gb.sellerysc.expense.ExpenseToPayData;
 import com.gb.sellerysc.salary.SalaryData;
 import com.gb.sellerysc.shared.utils.SectionTypeEnum;
@@ -20,11 +19,10 @@ public class CustomerData {
     private String username;
 
     private String email;
-
-    private List<ExpenseData> expenseList;
-
+    //Payment List
     private List<SalaryData> salaryList;
-
+    //Only that expense can be mapped to an expenseToPay
+    //which has enough place to have
     private List<ExpenseToPayData> expenseToPayList;
 
     private SectionTypeEnum mainSection;
